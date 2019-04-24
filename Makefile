37f2631ee159df2961076d53e79573c44d08e27d
sgupta1025 <samir@vcf1.cs.uga.edu>
2019-04-24 17:06:45 -0400

    Added stuff

---
+++ Makefile
@@ -0,0 +1,9 @@
+CC = g++
+LFLAGS = -Wall -std=c++14 -g -O0 -pedantic-errors 
+all: launcher tokeit
+
+tokeit: tokeit.cpp
+	$(CC) $(LFLAGS) -o tokeit tokeit.cpp
+
+launcher: launcher.cpp
+	$(CC) $(LFLAGS) -o launcher launcher.cpp
--- launcher.cpp
+++ launcher.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-#include <strings.h>
+#include <string>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <cstring>
 
 using namespace std;
 
 int main()
 {
+	string command;
+	cin >> command;
+	if(fork() ==0)
+	{
+		cout << "in child" << endl;
+		execlp(command.c_str(), command.c_str(), (char *) NULL);
+	}
+	else
+	{
+		int status;
+		int childID = wait(&status);
+		cout << "Waiting on child: " << childID << endl;
+		if(WIFEXITED(status) == true)
+		{
+			cout << "termination by exit" << endl;
+
+
+		}
+		if(WIFSIGNALED(status) == true)
+		{
+			cout << "termination by signal" << endl;
+		}
+	}
 
 }
